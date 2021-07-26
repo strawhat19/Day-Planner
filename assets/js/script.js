9f369765679062e2f7a92b01448daba9e33918ca
Rakib Ahmed <rakib987@gmail.com>
2021-07-26 01:27:12 -0400

    Hour Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,45 @@ var hours = [];
 var currentDay = $('#currentDay');
 var hourRowContainer = $('.hourRowsContainer');
 
-var todayObject = moment().toObject();
-console.log(todayObject);
+// -----------------------------------Generating Dynamic Hour Rows -------------------------------- //
+// Hours in Work Day
+var timeFormat = 'h A';
+// var rightNow = moment().format(timeFormat);
+var rightNow = moment();
+var startDay = moment('09', timeFormat).format(timeFormat); // Getting 9am as starting the day
+var startDay2 = moment('09'); // Getting 9am as starting the day
+var tenAm = moment('10', timeFormat).format(timeFormat);
+var elevenAm = moment('11', timeFormat).format(timeFormat);
+var twelvePm = moment('12', timeFormat).format(timeFormat);
+var onePm = moment('13', timeFormat).format(timeFormat);
+var twoPm = moment('14', timeFormat).format(timeFormat);
+var threePm = moment('15', timeFormat).format(timeFormat);
+var fourPm = moment('16', timeFormat).format(timeFormat);
+var endDay = moment('17', timeFormat).format(timeFormat); // Setting 5pm as end the day
+
+var test = moment().format('h');
+if (startDay2.isBefore(rightNow)) {
+    console.log(startDay2.format('h A'));
+}
+
+// Storing hours of day in Array
+var workDayHours = [startDay,tenAm,elevenAm,twelvePm,onePm,twoPm,threePm,fourPm,endDay];
+
+// Creating dynamic rows for each Hour
+workDayHours.forEach(hour => {
+    var hourRow = $('<div class="hourRow">');
+    hourRow.attr('data-value',hour);
+    hourRow.html(hour);
+    hourRowContainer.append(hourRow);
+    // var hourTimeValue = hourRow.html().split(' ');
+    // console.log(hourTimeValue[0]);
+
+})
+
+// If time
+
+// -----------------------------------End Generating Dynamic Hour Rows -------------------------------- //
+
 
 // Update Todays Time
 var today = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
@@ -19,18 +56,3 @@ var todayUpdate = setInterval(function() {
     var today = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
     currentDay.html(today);
 }, 1000);
-
-
-// Come back to this
-for (var i = 9; i < 17; i++){
-    var hourRow = $('<div class="hourRow">');
-    hourRow.html(i);
-    // if (i > 12) {
-    //     i = 0;
-    // }
-    hourRowContainer.append(hourRow);
-}
-
-
-
-
